32314a3a4490bf314f77621d56c84ee92caebc35
Mrcloudz49 <shawneealex49@gmail.com>
2023-04-17 12:04:36 -0700

    main file

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * main - shell main func
+ * @ac: unused
+ * @argv: pointer list of args
+ * Return: 0
+ */
+int main(__attribute__((unused))int ac, char **argv)
+{
+        int tty = 0;
+
+        tty = isatty(STDIN_FILENO);
+        if (tty == 2)
+        {
+                perror(argv[0]);
+                return (2);
+        }
+        else
+                shell(argv, tty);
+
+        return (0);
+}
+
