66d99a4374252d5843ec3069c7d4a7f6b8af75d0
Mrcloudz49 <shawneealex49@gmail.com>
2023-04-17 18:07:44 -0700

    testing

---
+++ prepstring.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * prep_string - takes a string, replaces newline with null byte
+ * @cmd: command to prep
+ * Return: pointer to frist char of cmd
+ */
+char *prep_string(char *cmd)
+{
+        int i = 0;
+        while (*cmd == ' ')
+        cmd++;
+        for (i = 0; i < _strlen(cmd); i++)
+        {
+                if (cmd[i] == '\n' || cmd[i] == ' ')
+                {
+                        cmd[i] = '\0';
+                        return (cmd);
+                }
+        }
+        return (cmd);
+}
